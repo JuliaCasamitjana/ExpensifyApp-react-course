e617ce8031c9e1e9a6aba79f58206b393c4b448e
Júlia Casamitjana Espunya <casamitjana.julia@gmail.com>
2020-05-04 16:08:19 +0200

    Setup production build and server

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import getVisibleExpenses from './selectors/expenses';
 
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
-import 'react-dates/lib/css/_Datepicker.css';
+import 'react-dates/lib/css/_datepicker.css';
 
 
 const store = configureStore();
